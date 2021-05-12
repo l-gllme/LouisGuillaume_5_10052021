9a4132d0267fedd56e88286d528b484bfdfeb2dc
Louis <louisguillaume000@gmail.com>
2021-05-13 00:44:08 +0200

    50% JS

--- JS/card.js
+++ JS/card.js
@@ -11,17 +11,35 @@ const name = document.getElementById('name');
 const description = document.getElementById('description');
 const price = document.getElementById('price');
 
+const optionSelect = document.getElementById('option-select');
+
 fetch('http://localhost:5000/api/teddies/' + id)
 	.then(res => {
 		if(res.ok){
 			res.json().then(data =>{
 				console.log(data);
 				img.src = data.imageUrl
-				price.innerHTML = data.price/100 + "€"
+				price.innerHTML = "Prix: "+data.price/100 + "€"
 				name.innerHTML = data.name
 				description.innerHTML = data.description
+
+				for(i in data.colors){
+					const newOption = document.createElement("option");
+					newOption.value = data.colors[i]
+					newOption.innerHTML = data.colors[i]
+					optionSelect.appendChild(newOption);
+				}
 			})
 		}else{
 			console.log("Error");
 		}
 	})
+
+const sendToBasket = document.getElementById('add-basket');
+let storage = 1 ;
+
+sendToBasket.addEventListener('click',() => {
+	alert("Produit ajouté au panier !")
+	localStorage.setItem(storage, id);
+	storage++;
+});
